8b9e8115d887c4f76b1c497c5364f9c992e417cb
unknown <driveaxle@gmail.com>
2018-12-17 21:25:42 +0500

    init

---
+++ BiaEdu/Controllers/HomeController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BiaEdu.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+           // ViewBag.Message = "Know us before the point of no return.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+          //  ViewBag.Message = ".";
+
+            return View();
+        }
+        public ActionResult Jobs()
+        {
+            //ViewBag.Message = ".";
+
+            return View();
+        }
+    }
+}
